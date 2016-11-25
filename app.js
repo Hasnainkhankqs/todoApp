04e060a97be8f2d90edc7108f2d8ff6156b3f29e
Hasnain Khan <Hasnainkhankqs@gmail.com>
2016-11-25 22:19:51 +0500

    modify

--- app.js
+++ app.js
@@ -16,14 +16,8 @@ function addTodo(){
     }
     var data = ""; 
     for(i = 0; i<listArray.length ; i++){
-    data = data + "<li>"+listArray[i]+' <input type="button" value="button" onclick="remove();"/>'+"</li>";
+    data = data + "<li>"+listArray[i]+' <input type="button" class="zain" value="button" onclick="remove();"/>'+"</li>";
     document.getElementById("todoList").innerHTML = data;
-    // console.log(data)
-    function remove(){
-        listArray.splice(i,1);
-        }
-
-
     document.getElementById("todoInput").value = "";
     }
 }
@@ -32,7 +26,3 @@ function Remove(){
     listArray = [];
     var listHeading = document.getElementById("todoHeading").innerHTML  ="All lists have been deleted!!"
 }
-
-
-
-
