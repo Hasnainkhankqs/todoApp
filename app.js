54b86a2180096dbd3eeef9d576c8d2c8fbf0c24e
Haider <headeralishah@gmail.com>
2016-11-26 16:25:07 +0500

    Delete functionality implemented.

--- app.js
+++ app.js
@@ -1,36 +1,40 @@
 var listArray = []; // create an empty array
-function addTodo(){
+function addTodo() {
     var takeInput = document.getElementById("todoInput").value;
-    if(takeInput === "" || takeInput === " " || takeInput === undefined){
+    if (takeInput === "" || takeInput === " " || takeInput === undefined) {
         alert("Please write something");
     }
-    else{
+    else {
         listArray.push(takeInput);
         // alert(listArray);
     }
-    if(listArray.length == 1){
+    if (listArray.length == 1) {
         var listHeading = document.getElementById("todoHeading").innerHTML = "YOU ENTERED THE FOLLOWING LIST";
     }
-    else if(listArray.length > 1 ){
-    var listHeading = document.getElementById("todoHeading").innerHTML = "YOU ENTERED THE FOLLOWING LISTS";
+    else if (listArray.length > 1) {
+        var listHeading = document.getElementById("todoHeading").innerHTML = "YOU ENTERED THE FOLLOWING LISTS";
     }
-    var data = ""; 
-    for(i = 0; i<listArray.length ; i++){
-    data = data + "<li>"+listArray[i]+' <input type="button" value="button" onclick="remove();"/>'+"</li>";
-    document.getElementById("todoList").innerHTML = data;
-    // console.log(data)
-    function remove(){
-        listArray.splice(i,1);
-        }
-
-
-    document.getElementById("todoInput").value = "";
+    var data = "";
+    for (i = 0; i < listArray.length; i++) {
+        data = data + "<li id='" + i + "'>" + listArray[i] + ' <input type="button" value="button" onclick="removeoneItem(this.parentNode);"/>' + "</li>";
+        document.getElementById("todoList").innerHTML = data;
+        // console.log(data)
+        document.getElementById("todoInput").value = "";
     }
 }
-function Remove(){
+
+
+function removeoneItem(val) {
+    console.log(val);
+    console.log(val.id);
+    listArray.splice(val.id, 1);
+    val.parentNode.removeChild(val);
+}
+
+function Remove() {
     document.getElementById("todoList").innerHTML = "";
     listArray = [];
-    var listHeading = document.getElementById("todoHeading").innerHTML  ="All lists have been deleted!!"
+    var listHeading = document.getElementById("todoHeading").innerHTML = "All lists have been deleted!!"
 }
 
 
